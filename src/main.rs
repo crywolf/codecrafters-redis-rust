4ca2c7fd9d144e1c77b70d7e5e58a4a61104b8be
Vojtěch Toman <cry.wolf@centrum.cz>
2024-02-13 20:20:28 +0100

    Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_connection(stream)?;
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
+
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    let response = b"+PONG\r\n";
+    stream.write_all(response)?;
+    stream.flush()?;
+    Ok(())
 }
