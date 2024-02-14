bfb54f97b1c2a66fca6ab867f322a6a48965fa9f
Vojtěch Toman <cry.wolf@centrum.cz>
2024-02-14 13:05:11 +0100

    Handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,25 @@
-use std::{
-    io::{Read, Write},
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379")?;
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_connection(stream)?;
-            }
-            Err(e) => {
-                eprintln!("error: {}", e);
-            }
-        }
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::spawn(async move { handle_connection(stream).await });
     }
-
-    Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+async fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buf = [0; 64];
     let response = b"+PONG\r\n";
     loop {
-        let _ = stream.read(&mut buf)?;
+        let _ = stream.read(&mut buf).await?;
 
-        stream.write_all(response)?;
-        stream.flush()?;
+        stream.write_all(response).await?;
+        stream.flush().await?;
     }
 }
